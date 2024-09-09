e79b2b9500d16962cdde4bea3059ee0abc5c3cc0
Uday_Prabhas <udayprabhas005@gmail.com>
2024-09-10 00:26:10 +0530

    jhgh

---
+++ T.js
@@ -0,0 +1,15 @@
+let boxes=document.querySelectorAll(".box");
+let turn0=true;
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+        if(turn0){
+            box.innerText="O";
+            turn0=false;
+        }
+        else{
+            box.innerText="X";
+            turn0=true;
+        }
+        box.disabled=true;
+    })
+})
